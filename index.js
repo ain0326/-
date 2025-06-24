b9fbc110681949feb3111090aa006c75cf3c5d40
ain0326 <dbfltmzk0326@gmail.com>
2025-06-24 23:58:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const COMPANY_API_KEY = 'app-gJ4LatCpb2l63CaHL8qhM0ZX';
+const COMPANY_API_KEY = 'app-9iqdLrxUu7tGS3BP9fz4xqqc';
 const COMPANY_API_URL = 'https://internal.company.ai/api/recommend';
 
 app.use(cors());
